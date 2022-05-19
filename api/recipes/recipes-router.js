81d7cd61b84a9bd4173b5178edf07b0bb88af59e
laurensruiz <laurenruiz97@yahoo.com>
2022-05-19 12:35:24 -0400

    setup router

---
+++ api/recipes/recipes-router.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+
+//catchall
+router.use('*', (req, res, next) => {
+    res.json({api:'up'})
+})
+
+// error handling
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+      devAdvice: 'the code broke somewhere in the recipes router',
+      message: err.message,
+      stack: err.stack,
+    })
+  })
+
+module.exports = router
