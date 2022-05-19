2df6cf2a921c89870b6033d390ac98d2651df971
laurensruiz <laurenruiz97@yahoo.com>
2022-05-19 13:39:08 -0400

    fixed migrations

--- data/migrations/20220519171150_initial-migration.js
+++ data/migrations/20220519171150_initial-migration.js
@@ -19,10 +19,41 @@ exports.up = async function(knex) {
         table.string('ingredient_unit', 50)
     })
     .createTable('steps', table => {
-        table.increments()
+        table.increments('step_id')
+        table.string('step_text', 200).notNullable()
+        table.integer('step_number').notNullable
+        //need to point back to recipe - foreign key
+        table.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('recipe_id')
+            .inTable('recipes')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
     })
+    //step ingredients because proj wants us to see ingredients for that particular step not the recipe, check relationships. BUt since steps is already connected to recipe so it can still refer to it
+
+    //remember it references steps and ingredients
     .createTable('step_ingredients', table => {
-        table.increments()
+        //primary key
+        table.increments('step_ingredients_id')
+        table.float('quantity').notNullable
+        //foreign key references the step
+        table.integer('step_id')
+            .unsigned()
+            .notNullable()
+            .references('step_id')
+            .inTable('steps')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+        table.integer('ingredient_id')
+            .unsigned()
+            .notNullable()
+            .references('ingredient_id')
+            .inTable('ingredients')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+           
     })
 };
 
